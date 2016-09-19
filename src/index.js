9d3f15583cb00d1a3160f90b95d230102043441f
Marv <marvin.lutz.mail@gmail.com>
2016-09-19 18:26:24 +0200

    [ADD] /hack command little test to show Callback capabilities

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@
 
 const Telegram = require('telegram-node-bot');
 const TelegramBaseController = Telegram.TelegramBaseController;
-const tg = new Telegram.Telegram('BotTokenHere');
+let TelegramBotToken = null;
+if(!TelegramBotToken){
+    if(process.argv[2]){
+        TelegramBotToken = process.argv[2];
+    } else {
+        console.log("No TelegramBot Token specified.");
+        process.exit(1);
+    }
+}
+const tg = new Telegram.Telegram(TelegramBotToken);
 
 const ip = require('ip');
 const publicIp = require('public-ip');
-
 
 
 const userData = function(user){
@@ -16,16 +24,17 @@ const userData = function(user){
 const helpText = "" +
     "Help Menu - Commands:\n" +
     "/ip : get current local ip address\n" +
-    "/wan : get current wan address";
+    "/wan : get current wan ip address\n" +
+    "/hack : experimental stuff";
 
 class StartController extends TelegramBaseController {
     handle($){
         let cmd = $._update._message._text;
         let user = $._update._message._from;
-        let debugName = "[/start]";
+        let debugName = "[otherwise]";
         console.log(debugName + "Request from " + userData(user));
 
-        $.sendMessage("Welcome "+ user._firstName +"\n\n" + helpText);
+        $.sendMessage("Welcome " + user._firstName + "\n\n" + helpText);
     }
 }
 class OtherwiseController extends TelegramBaseController {
@@ -77,11 +86,84 @@ class WanController extends TelegramBaseController {
     };
 }
 
+class XXX extends TelegramBaseController {
+    wanHandler($){
+        let user = $._update._message._from;
+        let debugName = "[/wan]";
+        console.log(debugName + "Request from " + userData(user));
+
+        if(user._id != 36710052){
+            $.sendMessage('You are not authorized to get this information, ' + user._firstName + " " + user._lastName);
+            return
+        }
+
+        publicIp.v4().then(ip =>{
+            $.sendMessage('WAN: ' + ip);
+        });
+    };
+
+    get routes(){
+        return {
+            '/wan': 'wanHandler'
+        }
+    };
+}
+
+
+let userList = {};
+let createHackSession = function(interval, $){
+    return setInterval(function(){
+        $.sendMessage(getDateTime());
+        // $.sendPhoto({ path: 'data/hacker.jpg'});
+        // $.sendDocument({ path: 'data/hacker.html'});
+    }, interval);
+};
+function getDateTime(){
+    var date = new Date();
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+    var min = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+    var sec = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+    var day = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+    return hour + ":" + min + ":" + sec + " - " + day + "." + month + "." + year;
+}
+
+class HackerController extends TelegramBaseController {
+    hackHandler($){
+        let user = $._update._message._from;
+        let debugName = "[/hack]";
+        console.log(debugName + "Request from " + userData(user));
+
+        if(userList.hasOwnProperty(user._id)){
+            clearInterval(userList[user._id]['callback']);
+            console.log(debugName + "Removed " + userData(user));
+            $.sendMessage("You were removed from /hack .");
+        } else {
+            userList[user._id] = {'callback': createHackSession(5000, $)};
+            console.log(debugName + "Added " + userData(user));
+            $.sendMessage("You were added to /hack .\nIf you want to be removed, just send /hack again.");
+        }
+    };
+
+    get routes(){
+        return {
+            '/hack': 'hackHandler'
+        }
+    };
+}
+
 tg.router
     .when(['/start'], new StartController())
     .when(['/help'], new StartController())
     .when(['/ip'], new IPController())
     .when(['/wan'], new WanController())
+    .when(['/hack'], new HackerController())
     .otherwise(new OtherwiseController())
 
 console.log("TelegramBot running...");
