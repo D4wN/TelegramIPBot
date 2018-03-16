f7cfb5fc85ed7841baa82bee045ed187854e9305
Marvin Lutz <marvin.lutz.mail@gmail.com>
2018-03-16 17:29:24 +0100

    [ADD] Ubisoft 2FA one-time code generation

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@
 const Telegram = require('telegram-node-bot');
 const TelegramBaseController = Telegram.TelegramBaseController;
 let TelegramBotToken = null;
-if(!TelegramBotToken){
-    if(process.argv[2]){
+let ubiSecret = null;
+if (!TelegramBotToken) {
+    if (process.argv[2]) {
         TelegramBotToken = process.argv[2];
     } else {
         console.log("No TelegramBot Token specified.");
@@ -13,11 +14,19 @@ if(!TelegramBotToken){
 }
 const tg = new Telegram.Telegram(TelegramBotToken);
 
+if (!ubiSecret) {
+    if (process.argv[3]) {
+        ubiSecret = process.argv[3];
+    } else {
+        console.log("No Ubisoft 2FA Secret specified.");
+    }
+}
+
 const ip = require('ip');
 const publicIp = require('public-ip');
 
 
-const userData = function(user){
+const userData = function (user) {
     return user._firstName + " " + user._lastName + "(" + user._id + ")"
 }
 
@@ -25,14 +34,16 @@ const helpText = "" +
     "Help Menu - Commands:\n" +
     "/ip : get current local ip address\n" +
     "/wan : get current wan ip address\n" +
+    "/ubi : get current ubisoft 2FA code\n" +
     "/hack : experimental stuff\n" +
     "/remind type value message\n" +
     "    type    = hour, min or date\n" +
     "    value   = the time value, or Date as: 'DD.MM.YYYY hh:mm'\n" +
     "    message = The reminder message you will get";
 
+
 class StartController extends TelegramBaseController {
-    handle($){
+    handle($) {
         let cmd = $._update._message._text;
         let user = $._update._message._from;
         let debugName = "[otherwise]";
@@ -42,7 +53,7 @@ class StartController extends TelegramBaseController {
     }
 }
 class OtherwiseController extends TelegramBaseController {
-    handle($){
+    handle($) {
         let cmd = $._update._message._text;
         let user = $._update._message._from;
         let debugName = "[otherwise]";
@@ -53,7 +64,7 @@ class OtherwiseController extends TelegramBaseController {
 }
 
 class IPController extends TelegramBaseController {
-    ipHandler($){
+    ipHandler($) {
         let user = $._update._message._from;
         let debugName = "[/ip]";
         console.log(debugName + "Request from " + userData(user));
@@ -61,29 +72,29 @@ class IPController extends TelegramBaseController {
         $.sendMessage('Local IP: ' + ip.address());
     };
 
-    get routes(){
+    get routes() {
         return {
             '/ip': 'ipHandler'
         }
     };
 }
 class WanController extends TelegramBaseController {
-    wanHandler($){
+    wanHandler($) {
         let user = $._update._message._from;
         let debugName = "[/wan]";
         console.log(debugName + "Request from " + userData(user));
 
-        if(user._id != 36710052){
+        if (user._id != 36710052) {
             $.sendMessage('You are not authorized to get this information, ' + user._firstName + " " + user._lastName);
             return
         }
 
-        publicIp.v4().then(ip =>{
+        publicIp.v4().then(ip => {
             $.sendMessage('WAN: ' + ip);
         });
     };
 
-    get routes(){
+    get routes() {
         return {
             '/wan': 'wanHandler'
         }
@@ -91,14 +102,14 @@ class WanController extends TelegramBaseController {
 }
 
 let userList = {};
-let createHackSession = function(interval, $){
-    return setInterval(function(){
+let createHackSession = function (interval, $) {
+    return setInterval(function () {
         $.sendMessage(getDateTime());
         // $.sendPhoto({ path: 'data/hacker.jpg'});
         // $.sendDocument({ path: 'data/hacker.html'});
     }, interval);
 };
-function getDateTime(){
+function getDateTime() {
     var date = new Date();
     var hour = date.getHours();
     hour = (hour < 10 ? "0" : "") + hour;
@@ -114,32 +125,31 @@ function getDateTime(){
     return hour + ":" + min + ":" + sec + " - " + day + "." + month + "." + year;
 }
 class HackerController extends TelegramBaseController {
-    hackHandler($){
+    hackHandler($) {
         let user = $._update._message._from;
         let debugName = "[/hack]";
         console.log(debugName + "Request from " + userData(user));
 
-        if(userList.hasOwnProperty(user._id)){
+        if (userList.hasOwnProperty(user._id)) {
             clearInterval(userList[user._id]['callback']);
             console.log(debugName + "Removed " + userData(user));
             $.sendMessage("You were removed from /hack .");
         } else {
-            userList[user._id] = {'callback': createHackSession(5000, $)};
+            userList[user._id] = { 'callback': createHackSession(5000, $) };
             console.log(debugName + "Added " + userData(user));
             $.sendMessage("You were added to /hack .\nIf you want to be removed, just send /hack again.");
         }
     };
 
-    get routes(){
+    get routes() {
         return {
             '/hack': 'hackHandler'
         }
     };
 }
 
-
 class ReminderController extends TelegramBaseController {
-    handle($){
+    handle($) {
         let user = $._update._message._from;
         let debugName = "[/remind]";
         console.log(debugName + "Request from " + userData(user));
@@ -149,7 +159,7 @@ class ReminderController extends TelegramBaseController {
         let typeCoefficient = 1;
 
         let interval = parseInt(data.value);
-        switch(data.type.toLowerCase()){
+        switch (data.type.toLowerCase()) {
             case 'h':
             case 'hour':
                 typeName = hours;
@@ -157,11 +167,11 @@ class ReminderController extends TelegramBaseController {
             case 'm':
             case 'min':
             case 'minute':
-                if(!interval){
+                if (!interval) {
                     $.sendMessage("Value was not a Number!");
                     break;
                 }
-                setTimeout(function(){
+                setTimeout(function () {
                     $.sendMessage("Hey, there was something to do: " + data.message);
                 }, (interval * 60 * 1000 * typeCoefficient));
                 $.sendMessage("You will be reminded in " + interval + " " + typeName + ".");
@@ -177,11 +187,39 @@ class ReminderController extends TelegramBaseController {
     }
 }
 
+//Ubisoft 2FA
+var ubi = require('speakeasy');
+class Ubisoft2FAController extends TelegramBaseController {
+    ubiHandle($) {
+        let user = $._update._message._from;
+        let debugName = "[/ubi]";
+        console.log(debugName + "Request from " + userData(user));
+        if (ubiSecret == null) {
+            $.sendMessage('Ubisoft2FAController: No Ubisoft Secret defined!');
+
+        } else {
+            let oneTimeCode = ubi.totp({
+                'secret': ubiSecret,
+                'encoding': "base32"
+            });
+            $.sendMessage('Ubisoft One-time Code: ' + oneTimeCode);
+        }
+    };
+
+    get routes() {
+        return {
+            '/ubi': 'ubiHandle'
+        }
+    };
+}
+
+
 tg.router
     .when(['/start'], new StartController())
     .when(['/help'], new StartController())
     .when(['/ip'], new IPController())
     .when(['/wan'], new WanController())
+    .when(['/ubi'], new Ubisoft2FAController())
     .when(['/hack'], new HackerController())
     .when(['/remind :type :value :message'], new ReminderController())
     .when(['/rm :type :value :message'], new ReminderController())
